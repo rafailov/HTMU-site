052874331910ad92d91a65fa8ad322daa73448e5
Denis <alarma7a@gmail.com>
2014-08-28 16:01:09 +0300

    update map

--- contacts.php
+++ contacts.php
@@ -21,167 +21,22 @@ require_once('header.html');
                                         </style>
                                         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
                                         <script>
-                                           /* function initialize() {
-                                                var myLatlng = new google.maps.LatLng(42.655394, 23.358625);
-                                                var mapOptions = {
-                                                    zoom: 14,
-                                                    icon: 'img/markerIcon.png',
-                                                    center: myLatlng
-                                                }
-                                            var stylesArray =
-                                                [
-                                                {
-                                                "featureType": "road.local",
-                                                "elementType": "labels.text.fill",
-                                                "stylers": [
-                                                { "visibility": "on" },
-                                                { "color": "#244464" }
-                                                ]
-                                                }, {
-                                                "featureType": "poi.business",
-                                                "elementType": "labels.text.fill",
-                                                "stylers": [
-                                                { "color": "#224566" }
-                                                ]
-                                                }, {
-                                                "featureType": "poi.government",
-                                                "elementType": "labels.text",
-                                                "stylers": [
-                                                { "color": "#244566" }
-                                                ]
-                                                }, {
-                                                "featureType": "road.local",
-                                                "elementType": "labels.text.fill",
-                                                "stylers": [
-                                                { "color": "#224564" }
-                                                ]
-                                                }, {
-                                                "featureType": "road.arterial",
-                                                "elementType": "labels.text.fill",
-                                                "stylers": [
-                                                { "color": "#224464" }
-                                                ]
-                                                }, {
-                                                "featureType": "landscape.man_made",
-                                                "elementType": "geometry.fill",
-                                                "stylers": [
-                                                { "color": "#fbfff9" }
-                                                ]
-                                                }, {
-                                                "featureType": "poi.park",
-                                                "elementType": "geometry.fill",
-                                                "stylers": [
-                                                { "color": "#28b05f" }
-                                                ]
-                                                }, {
-                                                "featureType": "poi.sports_complex",
-                                                "elementType": "geometry.fill",
-                                                "stylers": [
-                                                { "color": "#2c3d51" }
-                                                ]
-                                                }, {
-                                                "featureType": "poi.business",
-                                                "elementType": "geometry.fill",
-                                                "stylers": [
-                                                { "color": "#28ae60" }
-                                                ]
-                                                }, {
-                                                "featureType": "water",
-                                                "elementType": "geometry.fill",
-                                                "stylers": [
-                                                { "color": "#1982da" }
-                                                ]
-                                                }, {
-
-                                                }
-                                            ];
-
-                                                var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-                                                var marker = new google.maps.Marker({
-                                                    position: myLatlng,
-                                                    map: map,
-                                                    styles: stylesArray,
-                                                    scaleControl: true,
-                                                    streetViewControl: true,
-                                                    overviewMapControl: true
-                                                    title: 'Химикотехнологичен и Металургичен Университет'
-                                                });
-                                            }
-
-                                            google.maps.event.addDomListener(window, 'load', initialize);*/
-
                                            function initialize() {
                                                var coords = new google.maps.LatLng(42.655394, 23.358625);
-                                               var center = new google.maps.LatLng(42.654080, 23.352723);
+                                               var center = new google.maps.LatLng(42.654083, 23.350469);
 
                                                var stylesArray =
-                                                   [
-                                                       {
-                                                           "featureType": "road.local",
-                                                           "elementType": "labels.text.fill",
-                                                           "stylers": [
-                                                               { "visibility": "on" },
-                                                               { "color": "#244464" }
-                                                           ]
-                                                       }, {
-                                                       "featureType": "poi.business",
-                                                       "elementType": "labels.text.fill",
-                                                       "stylers": [
-                                                           { "color": "#224566" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "poi.government",
-                                                       "elementType": "labels.text",
-                                                       "stylers": [
-                                                           { "color": "#244566" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "road.local",
-                                                       "elementType": "labels.text.fill",
-                                                       "stylers": [
-                                                           { "color": "#224564" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "road.arterial",
-                                                       "elementType": "labels.text.fill",
-                                                       "stylers": [
-                                                           { "color": "#224464" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "landscape.man_made",
-                                                       "elementType": "geometry.fill",
-                                                       "stylers": [
-                                                           { "color": "#fbfff9" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "poi.park",
-                                                       "elementType": "geometry.fill",
-                                                       "stylers": [
-                                                           { "color": "#28b05f" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "poi.sports_complex",
-                                                       "elementType": "geometry.fill",
-                                                       "stylers": [
-                                                           { "color": "#2c3d51" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "poi.business",
-                                                       "elementType": "geometry.fill",
-                                                       "stylers": [
-                                                           { "color": "#28ae60" }
-                                                       ]
-                                                   }, {
-                                                       "featureType": "water",
-                                                       "elementType": "geometry.fill",
-                                                       "stylers": [
-                                                           { "color": "#1982da" }
-                                                       ]
-                                                   }, {
-
-                                                   }
-                                                   ];
+                                                   [{ "featureType": "road.local","elementType": "labels.text.fill", "stylers": [ { "visibility": "on" }, { "color": "#244464" }] },
+                                                   {"featureType": "poi.business","elementType": "labels.text.fill", "stylers": [ { "color": "#224566" }]},
+                                                   {"featureType": "poi.government","elementType": "labels.text","stylers": [{ "color": "#244566" }]},
+                                                   { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [ { "color": "#224564" } ]},
+                                                   { "featureType": "road.arterial", "elementType": "labels.text.fill", "stylers": [ { "color": "#224464" }]},
+                                                   {"featureType": "landscape.man_made", "elementType": "geometry.fill", "stylers": [{ "color": "#fbfff9" } ]},
+                                                   { "featureType": "poi.park","elementType": "geometry.fill", "stylers": [ { "color": "#28b05f" } ]},
+                                                   {"featureType": "poi.sports_complex", "elementType": "geometry.fill", "stylers": [ { "color": "#2c3d51" }]},
+                                                   { "featureType": "poi.business", "elementType": "geometry.fill",  "stylers": [ { "color": "#28ae60" } ] },
+                                                   { "featureType": "water", "elementType": "geometry.fill","stylers": [ { "color": "#1982da" }]},
+                                                   { } ];
 
                                                var mapOptions = {
                                                    zoom: 15,
@@ -213,7 +68,7 @@ require_once('header.html');
 
                                         </script>
 
-                                        <div class="contactInfo" ">
+                                        <div class="contactInfo">
                                             <h1>Контакти</h1>
                                             <address>1756 София, България</address>
                                             <address>Бул. Св. Климент Охридски 8</address>
