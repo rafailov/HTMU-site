5456da3857b0e115eccf5b7690aee6bff2fd0224
Denis <alarma7a@gmail.com>
2014-08-28 16:06:56 +0300

    fix

--- news.php
+++ news.php
@@ -4,7 +4,7 @@ require_once('header.html');
     <li ><a href="index.php" id="newsbutton" class="active">Новини</a></li>
     <li ><a href="For-us.php" id="forus" class="unActive">За Нас</a></li>
     <li ><a href="#" id="forStudents" class="unActive">За Студентите</a></li>
-    <li><a href="#" id="contacts" class="unActive">Контакти</a></li>
+    <li><a href="contacts.php" id="contacts" class="unActive">Контакти</a></li>
     <li><a href="#" id="faq" class="unActive">FAQ</a></li>
     </ul>
     </nav>
